9594159a5ff52abb419a31f46863dbf50fab6fdb
gabriel <ggonzalv@lion.lmu.edu>
2015-09-15 22:48:23 -0700

    implemented correct answers for all cards

--- js/bizingo-scripts.js
+++ js/bizingo-scripts.js
@@ -1,11 +1,15 @@
 $(function () {
-  var winners = [{name: "restaurant and cafe server", winner: ['c1','c2','c3','c4','c5']}];
-  var clicked = [];
-  var possibleWins = winners[0].winner.length;
+  var winners = [
+    ['c1','c2','c3','c4','c5'], //server
+    ['e1','e2','e3','e4','e5'], //loan coord
+    ['b1','b2','b3','b4','b5'], //scribe
+    ['a1','b2','c3','d4','e5'] //web dev
+  ];
   var totalCards = 4;
   var currentCard = 1;
-
-  $("#card" + currentCard).show();
+  var clicked = [];
+  var possibleWins = winners[currentCard - 1].length;
+  var isDisplaying = false;
 
   function emptyCells(){
     $(".bizingo-cell").css("background-image","none");
@@ -28,60 +32,56 @@ $(function () {
     clicked.push($(thisVal).attr('id'));
 
     var winCell = 0;
-
-    if($.inArray($(thisVal).attr("id"),winners[0].winner) > -1){
+    if($.inArray($(thisVal).attr("id"),winners[currentCard - 1]) > -1){
       $(thisVal).css("background-image","url('http://www.wpclipart.com/education/gold_stars/circle_star_gold_T.png')");
     } else {
       $(thisVal).css("background-image","url('http://www.iconsdb.com/icons/preview/gray/circle-xxl.png')");
     }
 
-    for(var i = 0; i < possibleWins;
-      i++){
-        if($.inArray(winners[0].winner[i], clicked) > -1) {
-          winCell++;
-        }
+    for(var i = 0; i < possibleWins; i++){
+      if($.inArray(winners[currentCard - 1][i], clicked) > -1) {
+        winCell++;
+      }
 
-        if(winCell === 5) {
-          displayOverlay();
-        }
+      if(winCell === 5) {
+        displayOverlay();
       }
     }
+  }
 
+  function displayOverlay(){
+    $(".bizingo-cell").toggleClass("flip");
+    $(".overlay").fadeTo("slow", 0.3,function(){
+      $(".overlay").css("visibility","visible");
+    }).fadeTo('slow',15);
+    winCell = 0;
+    clicked = [];
+  }
 
-    function displayOverlay(){
-      $(".bizingo-cell").toggleClass("flip");
+  function closeOverlay(){
+    $(".close-window").click(function(){
       $(".overlay").fadeTo("slow", 0.3,function(){
-        $(".overlay").css("visibility","visible");
+        $(".overlay").css("visibility","hidden");
       }).fadeTo('slow',15);
-      winCell = 0;
-      clicked = [];
-    }
-
-    function closeOverlay(){
-      $(".close-window").click(function(){
-        $(".overlay").fadeTo("slow", 0.3,function(){
-          $(".overlay").css("visibility","hidden");
-        }).fadeTo('slow',15);
-
-        $(".bizingo-cell").toggleClass("flip").css("background-image","none");
-      })
-    }
-
-
-    $('.bizingo-cell').click(function(){
-      checkWinner(this);
-      closeOverlay();
-    });
-
-      $("#right").click(function(){
-        $("#card" + currentCard).hide("slide",{},400,slideLeft);
-        emptyCells();
-        setTimeout(function() {return;},1000);
-      })
-
-      $("#left").click(function(){
-        $("#card" + currentCard).hide("slide",{direction:"right"},400,slideRight);
-        emptyCells();
-        setTimeout(function() {alert("ass")},1000);
+      $(".bizingo-cell").toggleClass("flip").css("background-image","none");
     })
+  }
+
+  $("#card" + currentCard).show();
+  closeOverlay();
+
+
+  $('.bizingo-cell').click(function(){
+    checkWinner(this);
   });
+
+  $("#right").click(function(){
+    $("#card" + currentCard).hide("slide",{},400,slideLeft);
+    emptyCells();
+  })
+
+  $("#left").click(function(){
+    $("#card" + currentCard).hide("slide",{direction:"right"},400,slideRight);
+    emptyCells();
+  })
+});
