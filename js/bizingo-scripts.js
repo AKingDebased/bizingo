87e90807f837224528e6758d64be220537dd5728
gabriel <ggonzalv@lion.lmu.edu>
2015-09-26 11:58:19 -0700

    add tooltips for card 4

--- js/bizingo-scripts.js
+++ js/bizingo-scripts.js
@@ -97,9 +97,37 @@ $(function () {
         $(this).wrap("<a title='Federal Housing Accessibility and Housing Accesibility Guidelines are federal statutes that dictate the protocols to be followed for handicap accesibility in any given establishment or building.'></a>");
       }
     }
+
+    if(currentCard === 4){
+      if($(this).attr("id") === "e2"){
+        $(this).wrap("<a title='Heating, Ventilation, & Air Conditioning licensed contractors are certified to install, repair, and maintain any and all varieties of heaters, ventilators, air conditioning units, and all other associated properties.'></a>");
+      }
+
+      if($(this).attr("id") === "c5"){
+        $(this).wrap("<a title='Human Capital Management is an approach to employee staffing that views people as assets (i.e. human capital) whose value can be measured and whose future value can be increased through investment.'></a>");
+      }
+
+      if($(this).attr("id") === "e3"){
+        $(this).wrap("<a title='Master&#39;s Degree in Nursing.  This is a step above registered nursing, and opens more career opportunities with higher salaries.  Not to be confused with the Microsoft Network.'></a>");
+      }
+
+      if($(this).attr("id") === "d5"){
+        $(this).wrap("<a title='The LAMP stack is a popular open source web platform used to run dynamic web sites and servers.  The name comes from its use of Linux, Apache server software, MySQL, and PHP/Python/Perl/whatever other serverside language of choice.'></a>");
+      }
+
+      if($(this).attr("id") === "c4"){
+        $(this).wrap("<a title='Adobe AfterEffects is a video editing software used for compositing, motion graphics design, and animation.  It can be used in close conjunction with Cinema 4D, a 3D modeling service that can be used to for texturing, rendering, and lighting in a 3D environment.'></a>");
+      }
+
+      if($(this).attr("id") === "a3"){
+        $(this).wrap("<a title='Photoshop and Illustrator are part of the Adobe software suite.  Each program contributes something to the design workflow, be it image editing or digital painting.'></a>");
+      }
+
+      if($(this).attr("id") === "e4"){
+        $(this).wrap("<a title='An Application Programming Interface is a set of routines and protocols used for building software applications, almost like an instruction manual.  An API based web app executes most of its functionality through the use of an API. This allows for functionality to be cross-compatible across phones, tablets, desktops, and otherwise.'></a>");
+      }
+    }
   });
-
-
 
 
   function emptyCells(){
